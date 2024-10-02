51edaca278c794ef198c5716d023b29ea5878346
hechmi10 <hechmi.jaidane@gmail.com>
2024-10-02 15:03:45 +0100

    terminer le projet

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef,HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
@@ -7,7 +7,10 @@ export class HighlightDirective {
 
   constructor(private el:ElementRef,private renderer:Renderer2) {}
 
-  ngAfterViewInit(){
+    @HostListener('mouseenter') onMouseEnter(){
     this.renderer.addClass(this.el.nativeElement,'highlight');
-  }
+    }
+    @HostListener('mouseleave') onMouseLeave(){
+      this.renderer.removeClass(this.el.nativeElement,'highlight');
+      }
 }
