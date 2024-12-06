c5f8ac5d677be81ad14b965e06de9247c5a8191d
hechmi10 <hechmi.jaidane@gmail.com>
2024-12-06 08:37:06 +0100

    Interaction entre les composants

--- src/app/list-products/list-products.component.ts
+++ src/app/list-products/list-products.component.ts
@@ -13,12 +13,12 @@ handleAddToShortlist($event: number) {
 throw new Error('Method not implemented.');
 }
   listProducts : Product2Module[]=[ 
-    {"id":1,"title":"Cuisinière","image":"assets/images/grand_electromenagers/cuisinière.jpeg", "description":"699 dt", "promotion":false, "quantity":50, "categoryId":1}, 
-    {"id":2,"title":"Réfrigérateur","image":"assets/images/grand_electromenagers/refrigerateur.jpeg", "description":"1500 dt", "promotion":false, "quantity":20, "categoryId":1}, 
-    {"id":3,"title":"Robot Pétrin","image":"assets/images/petit_electromenager/robot_petrin.jpeg", "description":"430 dt", "promotion":false, "quantity":150, "categoryId":2}, 
-    {"id":4,"title":"Fer à repasser", "image":"assets/images/petit_electromenager/fer_a_repasser.jpeg", "description":"130 dt", "promotion":false, "quantity":100, "categoryId":2}, 
-    {"id":5,"title":"Oppo","image":"assets/images/smart_phones/Oppo.jpeg", "description":"920 dt", "promotion":false, "quantity":10, "categoryId":4}, 
-    {"id":6,"title":"TV Téléfunkun", "image":"assets/images/tv_images_son/tv1.jpeg", "description":"845 dt", "promotion":false, "quantity":50, "categoryId":5}, 
+    {"id":1,"title":"Cuisinière","image":"assets/images/grand_electromenagers/cuisinière.png", "description":"699 dt", "promotion":false, "quantity":50, "categoryId":1}, 
+    {"id":2,"title":"Réfrigérateur","image":"assets/images/grand_electromenagers/refrigerateur.png", "description":"1500 dt", "promotion":false, "quantity":20, "categoryId":1}, 
+    {"id":3,"title":"Robot Pétrin","image":"assets/images/petit_electromenager/robot_petrin.png", "description":"430 dt", "promotion":false, "quantity":150, "categoryId":2}, 
+    {"id":4,"title":"Fer à repasser", "image":"assets/images/petit_electromenager/fer_a_repasser.png", "description":"130 dt", "promotion":false, "quantity":100, "categoryId":2}, 
+    {"id":5,"title":"Oppo","image":"assets/images/smart_phones/Oppo.png", "description":"920 dt", "promotion":false, "quantity":10, "categoryId":4}, 
+    {"id":6,"title":"TV Téléfunkun", "image":"assets/images/tv_images_son/tv1.png", "description":"845 dt", "promotion":false, "quantity":50, "categoryId":5}, 
   ] 
   shortList:ShortlistModule[]=[];
   idUser: number = 1;
